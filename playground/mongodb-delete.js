c1422bbc3bb8d193387940a8d46989fc3338262d
Kowshik Sundararajan <ksundararajan@authentic8.com>
2017-04-15 10:40:27 -0700

    Deleting mongodb documents

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,46 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		return console.log('* unable to connect to the database server');
+	}
+
+	console.log('* connected to database server');
+
+	/*
+		deleteMany documents
+	*/
+	db.collection('Todos').deleteMany({text:'DxE Speakout'}).then((result) => {
+		console.log(result);
+	});
+
+	/* 
+		deleteOne document 
+		- will target only the first document it encounters
+	*/
+	db.collection('Todos').deleteOne({text: 'DxE Speakout'}).then((result) => {
+		console.log(result);
+	});
+
+	/* 
+		findOneAndDelete 
+		- will target only the first document it encounters 
+		- different from deleteOne, because it returns the deleted document
+		
+	*/
+	db.collection('Todos').findOneAndDelete({text: 'DxE Speakout'}).then((result) => {
+		console.log(result);
+	})
+
+	
+	// Delete all users documents with name Kowshik
+	db.collection('Users').deleteMany({name: 'Kowshik'});
+
+	// Find user with that id, delete that document and return it
+	db.collection('Users').findOneAndDelete({
+		_id: new ObjectID('58f13d6d4913ef1a2f58047e')
+	}).then((results) => {
+		console.log(JSON.stringify(results, undefined, 2));
+	});
+});
--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -15,28 +15,47 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 		- find is a promise that can print the results in then()
 		- ObjectID is required to query by id because the id is an ObjectID not just a string
 	*/
-
-	// db.collection('Todos').find({
-	// 	_id: new ObjectID('58f13a803ee808194bdedcab')
-	// }).toArray().then((docs) => {
-	// 	console.log(JSON.stringify(docs, undefined, 2));
-	// }, (err) => {
-	// 	console.log('* unable to fetch todos', err);
-	// });
+	// Find all Todos documents with that specific id
+	db.collection('Todos').find({
+		_id: new ObjectID('58f13a803ee808194bdedcab')
+	}).toArray().then((docs) => {
+		console.log(JSON.stringify(docs, undefined, 2));
+	}, (err) => {
+		console.log('* unable to fetch todos', err);
+	});
 
 	// Query for the number of documents, returns count as successful arg
-	// db.collection('Todos').find().count().then((count) => {
-	// 	console.log(`Todos count: ${count}`);
-	// }, (err) => {
-	// 	console.log('* unable to fetch todos', err);
-	// });
+	db.collection('Todos').find().count().then((count) => {
+		console.log(`Todos count: ${count}`);
+	}, (err) => {
+		console.log('* unable to fetch todos', err);
+	});
 
 	// Query for a user by name
 	db.collection('Users').find({name: 'Kowshik'}).toArray().then((docs) => {
 		console.log(JSON.stringify(docs, undefined, 2));
 	}, (err) => {
 		console.log('* unable to fetch user', err);
-	})
+	});
 
+	// Insert many Todos documents
+	db.collection('Todos').insertMany([
+		{
+			text: 'DxE Speakout',
+			completed: false
+		},
+		{
+			text: 'DxE Speakout',
+			completed: false
+		},
+		{
+			text: 'DxE Speakout',
+			completed: false
+		}
+	]).then((results) => {
+		console.log(`* added ${results.insertedCount} todos`);
+	}, (err) => {
+		console.log('* unable to add todos', err);
+	});
 	// db.close();
 });
